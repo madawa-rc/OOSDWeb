ca436091dfa86d11f6646aefe8925382321db97e
isuruf <isuruf@gmail.com>
2013-10-02 13:41:30 +0530

    contact us

--- src/java/slmo/registration/Servlet/ContactFormServlet.java
+++ src/java/slmo/registration/Servlet/ContactFormServlet.java
@@ -5,30 +5,74 @@
 package slmo.registration.Servlet;
 
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import Mail.recieveMail;
+import java.io.PrintWriter;
 
-/**
- *
- * @author Fointar
- */
-public class ContactFormServlet extends HttpServlet{
-    private String name;
-    private String email;
-    private String message;
-    private String address;
+public class ContactFormServlet extends HttpServlet {
 
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, NoSuchAlgorithmException{
-        //response.setContentType("text/html;charset=UTF-8");
-        name = request.getParameter("name");
-        address = request.getParameter("address");
-        email = request.getParameter("email");
-        message = request.getParameter("message");
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        String email = request.getParameter("email");
+        String message = request.getParameter("message");
         recieveMail.recieveMail(name, address, email, message);
     }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
