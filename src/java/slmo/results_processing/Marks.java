17a1508c2495708514e08064b395d07f34145844
isuruf <isuruf@gmail.com>
2013-10-07 00:29:56 +0530

    marks

---
+++ src/java/slmo/results_processing/Marks.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slmo.results_processing;
+
+/**
+ *
+ * @author New
+ */
+public class Marks {
+    public void calculate(){
+        
+    }
+}
